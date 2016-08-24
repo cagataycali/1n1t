d37d1610ccfa46cb0829c3e9acfda2efe9c92b71
ccali14 <calicagatay@icloud.com>
2016-08-23 19:03:53 -0700

    Readme

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ var E = require('3x3c');
 var async = require('async');
 
 module.exports = function() {
-  B()
-    .then((value) => {return value})
-    .catch((err) => {
+  return new Promise(function (resolve, reject) {
+    B()
+      .then((value) => {resolve(value)})
+      .catch((err) => {
 
-      console.log('Not a git repository (or any of the parent directories): .git');
+        console.log('Not a git repository (or any of the parent directories): .git');
 
-      var prompt = require('prompt');
-      prompt.start();
-      prompt.get([{name:'url', required: true, description: "Git remote url: Ex. https://github.com/cagataycali/br4anch.git"}], function (err, result) {
-        E(`git init && git remote add origin ${result.url}`)
-          .then((value) => {return value})
-          .catch((err) => {return err});
+        var prompt = require('prompt');
+        prompt.start();
+        prompt.get([{name:'url', required: true, description: "Git remote url: Ex. https://github.com/cagataycali/br4anch.git"}], function (err, result) {
+          E(`git init && git remote add origin ${result.url}`)
+            .then((value) => {resolve(value)})
+            .catch((err) => {reject(err)});
+        });
       });
-    });
+  })
 }
