6679827ac04fdbc2085585b42a76b0481a277035
ccali14 <calicagatay@icloud.com>
2016-08-23 18:38:41 -0700

    Readme

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ var E = require('3x3c');
 var async = require('async');
 
 module.exports = function() {
-  B()
-    .then((value) => {resolve(value)})
-    .catch((err) => {
+  return new Promise(function () {
+    B()
+      .then((value) => {resolve(value)})
+      .catch((err) => {
 
-      console.log('Not a git repository (or any of the parent directories): .git');
+        console.log('Not a git repository (or any of the parent directories): .git');
 
-      var prompt = require('prompt');
-      prompt.start();
-      prompt.get([{name:'url', required: true, description: "Git remote url: Ex. https://github.com/cagataycali/br4anch.git"}], function (err, result) {
-        E(`git init && git remote add origin ${result.url} && git remote show origin`)
-          .then((value) => {console.log(value);})
-          .catch((err) => {console.log(reject(err));});
+        var prompt = require('prompt');
+        prompt.start();
+        prompt.get([{name:'url', required: true, description: "Git remote url: Ex. https://github.com/cagataycali/br4anch.git"}], function (err, result) {
+          E(`git init && git remote add origin ${result.url} && git remote show origin`)
+            .then((value) => {console.log(value);})
+            .catch((err) => {console.log(reject(err));});
+        });
       });
-    });
+  })
 }
