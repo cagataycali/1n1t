34a5cce7a8f519530b5c0b3bb14764fc3c7a8436
cagataycali <calicagatay@icloud.com>
2016-08-30 20:42:09 -0700

    Some fixes

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var B = require('br4nch');
 var E = require('3x3c');
 var async = require('async');
+var inquirer = require('inquirer');
 var updateNotifier = require('update-notifier');
 var pkg = require('./package.json');
 updateNotifier({pkg}).notify();
+var cmdify = require('cmdify');
+
+var loader = [
+  '/ Installing.',
+  '| Installing..',
+  '\\ Installing...',
+  '- Installing..'
+];
+var i = 4;
+var ui = new inquirer.ui.BottomBar({bottomBar: loader[i % 4]});
+
+setInterval(function () {
+  ui.updateBottomBar(loader[i++ % 4]);
+}, 300);
 
 module.exports = function() {
   return new Promise(function (resolve, reject) {
@@ -11,17 +26,22 @@ module.exports = function() {
       .then((value) => {resolve(value)})
       .catch((err) => {
 
-        console.log('Not a git repository (or any of the parent directories): .git');
+        var questions = [
+          {
+            type: 'input',
+            name: 'url',
+            message: 'What\'s your git repository url?'
+          }
+        ];
 
-        var prompt = require('prompt');
-        prompt.start();
-        prompt.get([{name:'url', required: true, description: "Git remote url: Ex. https://github.com/cagataycali/br4anch.git"}], function (err, result) {
-          E(`git init && git remote add origin ${result.url} && git remote show origin && git symbolic-ref HEAD && echo "# Hi" >> README.md && git add . && git commit -m "Hi" && git push -u origin master`)
-            .then((value) => {
-              B()
-                .then((out) => {resolve(out)})
-            })
-            .catch((err) => {reject(err)});
+        inquirer.prompt(questions).then(function (answers) {
+          E(`git init && git remote add origin ${answers.url} && git remote show origin && git symbolic-ref HEAD && echo "# Hi" >> README.md && git add . && git commit -m "Hi" && git push -u origin master`)
+              .then((value) => {
+                B()
+                  .then((out) => {ui.updateBottomBar('Init done!\n');resolve(out)})
+                  .catch((err) => {ui.updateBottomBar('Init error!\n', err);reject(err)})
+              })
+              .catch((err) => {ui.updateBottomBar('Init error!\n', err);reject(err)});
         });
       });
   })
