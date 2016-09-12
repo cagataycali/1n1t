241743a4c1a56833973316522774cc1e0f546907
cagataycali <calicagatay@icloud.com>
2016-09-11 17:04:02 -0700

    All codes refactored 📦

--- index.js
+++ index.js
@@ -1,48 +1,101 @@
-var B = require('br4nch');
-var E = require('3x3c');
 var async = require('async');
-var inquirer = require('inquirer');
+var E = require('3x3c');
+var B = require('br4nch');
+var C = require('c0mm1t');
+var colors = require('colors');
 var updateNotifier = require('update-notifier');
 var pkg = require('./package.json');
+var emoji = require('node-emoji');
+var inquirer = require('inquirer');
+var isGitUrl = require('is-git-url');
 updateNotifier({pkg}).notify();
-var cmdify = require('cmdify');
 
-module.exports = function() {
-  return new Promise(function (resolve, reject) {
-    B()
-      .then((value) => {resolve(value)})
-      .catch((err) => {
+function q(content) {
+  return new Promise(function(resolve, reject) {
+    inquirer.prompt(content)
+      .then((outputs) => {
+        resolve(outputs);
+      }).catch((err) => {reject(err)})
+  });
+}
 
-        var questions = [
-          {
-            type: 'input',
-            name: 'url',
-            message: 'What\'s your git repository url?'
+var contentFirst = [
+  {
+    type: 'confirm',
+    name: 'commitWithInit',
+    message: 'Would you want commit and initial push with init?',
+    default: true
+  },
+  {
+    type: 'input',
+    name: 'url',
+    message: 'What\'s your git url?',
+    validate: function (value) {
+      if (isGitUrl(value)) {
+        return true;
+      }
+
+      return 'Please enter a git url.';
+    }
+  }
+];
+
+var contentSecond = [
+  {
+    type: 'input',
+    name: 'message',
+    message: 'What\'s your awesome hello new repo commit?',
+    validate: function (value) {
+      if (value.trim().length > 0) {
+        return true;
+      }
+
+      return 'Please write down your best comments...';
+    }
+  }
+];
+
+function questions() {
+  return new Promise(function(resolve, reject) {
+    q(contentFirst)
+      .then((output) => {
+        if (output.commitWithInit) {
+          q(contentSecond)
+            .then((value) => {
+              var obj = {
+                message: value.message,
+                url: output.url,
+                new: true,
+              }
+              resolve(obj)
+            })
+            .catch((err) => {reject(err);})
+        } else {
+          var obj = {
+            message: 'Awesome repository initialized with g3l.',
+            url: output.url,
+            new: true,
           }
-        ];
+          resolve(obj)
+        }
+      })
+      .catch((err) => {reject(err);})
+  });
+}
 
-        inquirer.prompt(questions).then(function (answers) {
-          var loader = [
-            '/ Installing.',
-            '| Installing..',
-            '\\ Installing...',
-            '- Installing..'
-          ];
-          var i = 4;
-          var ui = new inquirer.ui.BottomBar({bottomBar: loader[i % 4]});
-
-          setInterval(function () {
-            ui.updateBottomBar(loader[i++ % 4]);
-          }, 300);
-
-          E(`git init && git remote add origin ${answers.url} && git remote show origin && git symbolic-ref HEAD && echo "# Hi" >> README.md && git add . && git commit -m "Hi" && git push -u origin master`)
-              .then((value) => {
-                B()
-                  .then((out) => {ui.updateBottomBar('Init done!\n');resolve(out)})
-                  .catch((err) => {ui.updateBottomBar('Init error!\n', err);reject(err)})
-              })
-              .catch((err) => {ui.updateBottomBar('Init error!\n', err);reject(err)});
-        });
-      });
-  })
+module.exports = function run() {
+  B()
+    .then((value) => {console.log(colors.red('Git already initialized.'));})
+    .catch((err) => {
+      questions()
+       .then((obj) => {
+         console.log(obj);
+         E(`git init; git remote add origin ${obj.url};`)
+          .then((value) => {
+            C(obj)
+             .then((value) => {resolve(value)})
+             .catch((err) => {reject(value)})
+          })
+       })
+    })
 }
